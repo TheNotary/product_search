cc6acf40310b480b182e3f7006c2f340b2b0d465
TheNotary <no@email.plz>
2016-06-22 18:42:11 -0700

    enabled caching

--- app/controllers/semantics_api_controller.rb
+++ app/controllers/semantics_api_controller.rb
@@ -4,14 +4,18 @@ class SemanticsApiController < ApplicationController
   def search
     query = params[:query]
 
-    # TODO: check if it's in cache
+    search_cache = SearchCache.find_by(query: query)
 
-    # if not execute query
-    if Rails.env == "test" and ENV['disable_querying_third_parties'] == 'true'
-      products_json_hash = File.read('spec/data/test_search_response.json')
+    if search_cache
+      # if we have a 'valid' cache, use it
+      products_json_hash = search_cache.response
     else
-      products_json_hash = File.read('spec/data/test_search_response.json')
-      # products_json_hash = run_search_against_semantic(query)
+      # if not execute query
+      if Rails.env == "test" and ENV['disable_querying_third_parties'] == 'true'
+        products_json_hash = File.read('spec/data/test_search_response.json')
+      else
+        products_json_hash = run_search_against_semantic(query)
+      end
     end
 
     search_result = JSON.parse(products_json_hash)
