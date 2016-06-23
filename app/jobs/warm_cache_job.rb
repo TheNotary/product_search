908f5e86907c35c50d4e990a328da022a4d10eb5
TheNotary <no@email.plz>
2016-06-23 16:02:43 -0700

    adds place in application.yml for precachable queries

--- app/jobs/warm_cache_job.rb
+++ app/jobs/warm_cache_job.rb
@@ -3,7 +3,7 @@ class WarmCacheJob < ActiveJob::Base
 
   def perform(*args)
     # Do something later
-    predefined_cache = ["Samsung Galaxy", "iPad", "iPhone"]
+    predefined_cache = ENV['precachable_queries'].split(",")
 
     predefined_cache.each do |query|
       Product.execute_search_against_semantic(query)
